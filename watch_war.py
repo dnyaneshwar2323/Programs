5c1ec63c43397ec269c8a3f448dd4204ce5041d0
DNYANESHWAR CHAVAN <101380335+dnyaneshwar2323@users.noreply.github.com>
2022-09-18 22:45:05 +0530

    Add files via upload
    Watch war movie who's name start with letter 'a' and age is greater than 20

---
+++ watch_war.py
@@ -0,0 +1,6 @@
+name=input("enter the name ")
+age=int(input("enter the age= "))
+if name[0]=='a'or name[0]=='A' and age>=20:
+    print("you can watch war movie")
+else :
+    print("sorry, You cannot watch war movie")
